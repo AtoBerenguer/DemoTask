15ea087b115c8f57d1b3fc16c8a2074bb8dcf4e2
Ato <ato19942@gmail.com>
2025-10-11 14:18:20 +0200

    Agregar métodos para obtener, actualizar y eliminar modelos en ModelController y ModelRepository

--- src/Controllers/ModelController.php
+++ src/Controllers/ModelController.php
@@ -17,6 +17,33 @@ use App\Repository\ModelRepository;
             return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
         }
         
+        public static function getAll(Request $request, Response $response)
+        {
+            $models = (new ModelRepository())->getAll();
+            if (empty($models))
+            {
+                $response->getBody()->write(json_encode(['message' => 'No models found']));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+            }
+            $response->getBody()->write(json_encode($models));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+        }
+        public function update(Request $request, Response $response){
+            $data = $request ->getParsedBody();
+            (new ModelRepository())->update($data);
+
+            $response->getBody()->write(json_encode(['message'=>'Modelo actualizada']));
+            return $response->withHeader('Content-Type','application/json')->withStatus(201);
+        }
+
+        public function delete(Request $request, Response $response){
+            $data = $request ->getParsedBody();
+            (new ModelRepository())->delete($data);
+
+            $response->getBody()->write(json_encode(['message'=>'Modelo borrada']));
+            return $response->withHeader('Content-Type','application/json')->withStatus(201);
+            
+        }
         
         
     };
--- src/Repository/ModelRepository.php
+++ src/Repository/ModelRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Connection\dbConnection;
 use PDO;
+use Error;
 
 class ModelRepository{
 private $db;
@@ -15,6 +16,34 @@ private $db;
     public function create(array $data) {
         $stmt = $this->db->prepare("INSERT INTO Model (Model_Name) VALUES (:Model_Name)");
         $stmt->bindParam(':Model_Name', $data['Model_Name']);
+        $stmt->execute();
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Model");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function update(array $data)
+    {
+        $stmt = $this->db->prepare("UPDATE Model SET Model_Name = :Model_Name WHERE Id = :Id");
+        if (empty($data['Model_Name'])){
+            throw new Error('Todos los campos son requeridos');}
+
+            $stmt->bindParam(':Id',$data['Id']);
+            $stmt->bindParam(':Model_Name', $data['Model_Name']);
+
+            $stmt->execute();
+        
+    }
+
+    public function delete(array $data)
+    {
+        $stmt = $this->db->prepare("DELETE FROM Model WHERE Id = :Id");
+        $stmt->bindParam(':Id', $data['Id']);
         $stmt->execute();
     }
    
